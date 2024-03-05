138e47e9e8e40705444280d3176fdc6b9f8dbae3
PrabhaK <kprabha0710@gmail.com>
2024-03-05 17:30:11 +0530

    ADDED : 10 - ConditionalStatements

---
+++ PrabhaK/03 - JavaScript/01 - Basics/10 - ConditionalStatements.js
@@ -0,0 +1,37 @@
+/*let number = prompt('enter a any number to guess number game ');
+if(number == 2015)
+{
+    console.log('you guessed right ');
+}
+else
+{
+    console.log('youre wrong');
+}
+
+*/
+let age = 1999;
+let accessAllowed = (age > 18) ? true : false;
+console.log(accessAllowed);
+
+
+//0r
+
+let accessAllowed1 = age > 18;
+console.log(accessAllowed1);
+
+
+if("0")
+{
+    console.log('helloo');
+}
+
+
+if("")
+{
+    console.log('helloo');
+}
+
+if("879065jgkh")
+{
+    console.log('helloo');
+}
