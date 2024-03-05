12527607994fe72508dd2139cf448659d69440c3
PrabhaK <kprabha0710@gmail.com>
2024-03-05 18:47:01 +0530

    ADDED : 11 - LoopingStatements

---
+++ PrabhaK/03 - JavaScript/01 - Basics/11 - LoopingStatements.js
@@ -0,0 +1,32 @@
+
+
+let i = 0;
+while(i < 3)
+{ // shows 0, then 1, then 2
+    console.log(i);
+    i++;
+}
+console.log("Next Program...")
+let j = 3;
+while(j)
+{ // when j becomes 0, the condition becomes falsy, and the loop stops
+    console.log(j);
+    j--;
+}
+console.log("Next Program...")
+
+let k = 0;
+do
+{
+    console.log(k);
+    k++;
+
+}
+while(k < 3);
+
+
+for(let n = 0; n < 3; n++)
+{
+   console.log(n); 
+}
+console.log(n);
