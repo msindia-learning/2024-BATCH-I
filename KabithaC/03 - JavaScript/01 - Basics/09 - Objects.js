12259187c0c92ae24a7c9daaf36af1dfbd711279
KabithaC <kabi.kabila.171@gmail.com>
2024-03-06 19:02:52 +0530

    ADDED:07-Arrays

---
+++ KabithaC/03 - JavaScript/01 - Basics/09 - Objects.js
@@ -0,0 +1,29 @@
+//object
+
+let car = "FLAT";
+console.log(car);
+
+//many objects
+let house = {
+    type: "newhouse",
+    flat: "Individual",
+    place: "anna nagar",
+    rentamount:10000
+};
+console.log(house);
+console.log(house.type);
+console.log(house.flat);
+console.log(house.place);
+console.log(house.rentamount);
+
+//this function
+const person = {
+    firstName: "Anees",
+    lastName :"fathima",
+    demo: function()
+    {
+        return this.firstName + " " +this. lastName;
+    }
+};
+
+console.log(person.demo());
