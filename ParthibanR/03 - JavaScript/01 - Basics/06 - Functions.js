fb3d55b72411b0c3162a315c0f030dd771c1cca8
ParthibanR <parthibanvijay467@gmail.com>
2024-03-08 18:44:35 +0530

    MODIFIED:01 - Basics

--- ParthibanR/03 - JavaScript/01 - Basics/06 - Functions.js
+++ ParthibanR/03 - JavaScript/01 - Basics/06 - Functions.js
@@ -30,3 +30,147 @@ function FunctionAge() {
 }
 
 document.write(FunctionAge());
+
+
+
+function myFunction(a, b) {
+    return a * b;
+}
+
+console.log(myFunction(10, 20));
+
+                                      //Arrow function
+
+
+
+
+
+
+
+
+
+
+let functionName = (a, b) => a * b;
+console.log(functionName(6, 8));
+
+
+
+let hello = () => {
+    return "Hello World..!!!";
+}
+console.log(hello());                             //Hello World..!!!
+
+
+
+const welcome = () => "hello world..!!!";
+console.log(welcome());                           //hello world..!!!
+
+
+var val = "Document object model";
+var hello = (val) => {
+    return "Hello"+ " " +val;
+}
+console.log(hello(val));
+
+
+var valuee ="Script"
+var welcome = () => "Java" + " " + valuee;
+
+console.log(welcome(valuee));
+
+
+
+let person = {
+    fname: "leo",
+    lname: "das",
+    age: 23,
+    degree: "Emgineering",
+    fullname: function () {
+        return (this.fname + " " + this.lname).toUpperCase();
+    }
+};
+person.fullname();
+console.log(person.fullname());
+
+
+           //function constructor
+
+function myFunction(arg1, arg2) {
+    this.firstname = arg1;
+    this.lastname = arg2;
+}
+
+const namesss = new myFunction("Leo", "Das");
+
+console.log(namesss.firstname);
+console.log(namesss.lastname);
+
+
+
+const person = {
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+}
+const person1 = {
+    firstName: "Antony",
+    lastName: "Das"
+}
+const person2 = {
+    firstName: "Hari",
+    lastName: "Das"
+}
+console.log(person.fullName.call(person1));
+console.log(person.fullName.call(person2)); 
+
+
+const person = {
+    fullName: function (city, country) {
+        return this.firstName + " " + this.lastName + "," + city + "," + country;
+    }
+}
+
+const person1 = {
+    firstName: "Leo",
+    lastName: "Das"
+}
+
+const person2 = {
+    firstName: "Anoty",
+    lastName: "Das"
+}
+
+console.log(person.fullName.call(person1, "Chennai", "India")); 
+
+
+const person = {
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const person1 = {
+    firstName: "Muruga",
+    lastName: "Das"
+}
+
+console.log(person.fullName.apply(person1)); 
+
+
+
+const person = {
+    firstName: "Ram",
+    lastName: "Das",
+    fullName: function () {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+const member = {
+    firstName: "Namah",
+    lastName: "Shivaya",
+}
+
+let fullName = person.fullName.bind(member);
+
+console.log(fullName());
