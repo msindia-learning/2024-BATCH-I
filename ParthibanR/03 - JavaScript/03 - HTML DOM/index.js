f3cf79b0f5f334032b532653f9db46a71887cc43
ParthibanR <parthibanvijay467@gmail.com>
2024-03-08 18:45:17 +0530

    MODIFIED:03 - HTML DOM

---
+++ ParthibanR/03 - JavaScript/03 - HTML DOM/index.js
@@ -0,0 +1,11 @@
+document.getElementById('demo').innerHTML = "Welcome to my js page.If you want to access any element in an HTML page, you always start with accessing the document object.Below are some examples of how you can use the document object to access and manipulate HTML";
+
+document.getElementById('demo').style.color = "blue";
+
+document.getElementById('demo').style.fontSize = "29px";
+
+document.getElementById('welcome').style.textAlign = "center";
+
+document.getElementById('demo').style.textIndent = "190px";
+
+
