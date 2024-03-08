e095b6b2954121121355a2b7cf3060011aa48edf
JeniferFathimaH <jeniferfathima2103@gmail.com>
2024-03-08 18:40:07 +0530

    ADDED: 03 - JS HTML DOM 05 - CSS

--- JenifarFathimaH/03 - JavaScript/03 - JS HTML DOM/06 - Events/script.js
+++ JenifarFathimaH/03 - JavaScript/03 - JS HTML DOM/06 - Events/script.js
@@ -1,9 +1,14 @@
+function changeColor() {
 
-   function validateForm() {
-       let x = document.forms["myForm"]["fname"].value;
-    if (x == "" ) {
-        alert("Name must be filled out");
-    return false;
-        }
-    i
-    }
+    var divElement = document.querySelector(".colorful-div");
+
+    var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    divElement.style.backgroundColor = randomColor;
+}
+
+function resetColor() {
+    var divElement = document.querySelector(".colorful-div");
+
+    divElement.style.backgroundColor = "lightblue";
+}
