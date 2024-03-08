f451c6b508ec4bf0560aa3b031d8051b161dd846
KabithaC <kabi.kabila.171@gmail.com>
2024-03-08 14:16:12 +0530

    ADDED: 12 - SetsAndMaps

--- KabithaC/03 - JavaScript/01 - Basics/12 - SetsAndMaps.js
+++ KabithaC/03 - JavaScript/01 - Basics/12 - SetsAndMaps.js
@@ -35,3 +35,50 @@ for(const x of js.values())
     text1 += x + "<br>";
 }
 console.log(text1);
+
+// create a map
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+]);
+console.log(fruits);
+
+
+// set method
+const colors = new Map();
+colors.set("nippon paints", 400);
+colors.set("asian paints", 300);
+colors.set("indigo paints", 350);
+console.log(colors);
+result = colors.get("nippon paints");
+console.log(result);
+
+//foreach
+
+const fru = new Map([
+    ["app", 500],
+    ["ban", 300],
+    ["ora", 200]
+]);
+
+let pan = "";
+fru.forEach(function(value, key)
+{
+  pan += key + ' = ' + value + "<br>"
+})
+console.log(pan);
+
+//entries
+const f = new Map([
+    ["an", 500],
+    ["bn", 300],
+    ["on", 200]
+]);
+
+let te = "";
+for(const x of f.entries())
+{
+    te += x + "<br>";
+}
+console.log(te);
---
+++ KabithaC/03 - JavaScript/01 - Basics/13 - TypeConversion.js
@@ -0,0 +1,44 @@
+const frnds = ["jeni", "kabi", "anees", "fazal",];
+function isArray(myArray)
+{
+    return myArray.constructor === Array;
+}
+console.log(isArray(frnds));
+
+
+//date object
+const myDate = new Date();
+function isDate(myDate)
+{
+    return myDate.constructor === Date;
+}
+console.log(isDate(myDate));
+
+//value undefined
+let car;
+console.log(car);
+
+//type undefined
+car = undefined;
+console.log(car);
+
+//empty values
+let van = " ";
+console.log(typeof (van));
+
+
+//string to number 
+
+let str = "123";
+let num = parseInt(str);
+console.log(num);
+
+// string to floatnumber
+let string = "11.789";
+let number = parseFloat(string);
+console.log(number); 
+
+
+//number to string
+let x = 123;
+console.log(x.toString());
