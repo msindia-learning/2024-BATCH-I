dd0911dce051ccd2f0d0617dc210530d98b5d2f9
PrabhaK <kprabha0710@gmail.com>
2024-03-12 18:41:22 +0530

    MODIFIED : 06 - Functions

--- PrabhaK/03 - JavaScript/01 - Basics/07 - Arrays.js
+++ PrabhaK/03 - JavaScript/01 - Basics/07 - Arrays.js
@@ -61,25 +61,118 @@ console.log(some);
 */
 
 //read --> Map method
-const value = Array.of(1, 2, 3, 4);
-value.map((x) => console.log(x));
+//const value = Array.of(1, 2, 3, 4);
+//value.map((x) => console.log(x));
 
-const newarray = value.map((x) => x * x);
-console.log(newarray);
+//const newarray = value.map((x) => x * x);
+//console.log(newarray);
 
-// filer() method
+//// filer() method
 
-const myarray = [1, 3, 6, 9, 12];
-console.log(myarray.filter((x) => x % 3 == 0));
+//const myarray = [1, 3, 6, 9, 12];
+//console.log(myarray.filter((x) => x % 3 == 0));
 
-const array = myarray.filter((item) => item > 4);
-console.log(array);
+//const array = myarray.filter((item) => item > 4);
+//console.log(array);
 
-delete myarray[2];
-console.log(myarray);
+//delete myarray[2];
+//console.log(myarray);
+//var fib = [];
+//fib[1] = 1;
+//fib[2] = 1;
+//for(var i = 3; i < 20; i++)
+//{
+//    fib[i] = fib[i - 2] + fib[i - 1];
+
+//}
+
+//for(var i = 1; i < fib.length; i++)
+//{ //{5}
+//    console.log(fib[i]); //{6}
+//}
+//fib.push(1 , 2345567);
+//console.log(fib[i])
+
+var numbers = [];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+//console.log(numbers);
+//numbers.push(2);
+//console.log(numbers);
+
+//numbers.unshift(2, 2, 2, 2);
+//console.log(numbers);
+
+//numbers.shift(numbers.length);
+//console.log(numbers);
+//c = numbers.splice(1, 4);
+//console.log(c);
+
+//console.log(numbers);
+
+//d = numbers.splice(3, 0, 876, 98876, 9876, 457687);
+//console.log(numbers);
+//console.log(d);
+
+//var averagetemp = [];
+//averagetemp[0] = [];
+//averagetemp[0][0] = 45;
+//averagetemp[0][1] = 90;
+//averagetemp[0][2] = 78;
+//averagetemp[1] = [];
+//averagetemp[1][0] = 34;
+//averagetemp[1][1] = 87;
+//averagetemp[1][2] = 56;
+
+//console.log(averagetemp);
+
+//function matrix(averagetemp)
+//{
+//    for(var i = 0; i < averagetemp.length; i++)
+//    {
+//        for(var j = 0; j < averagetemp[i].length; j++)
+//        {
+//            console.log(averagetemp[i][j]);
+
+//        }
+//    }
+
+//}
+//console.log(averagetemp[0].length);
+//var zero= 0;
+//var positive = [1, 2, 3, 4];
+//var negative = [5, 6, 7, 8];
+//var numbers = positive.concat(zero, negative);
+//console.log(numbers);
+//var number1 = negative.concat(zero, positive);
+//console.log(number1);
+
+var number = [];
+number = [11, 12, 34, 15,  1, 3, 45, 54, 189];
+console.log(number);
+//sorted = number.sort();
+//console.log(sorted);
+/*console.log(number.every(isEven));*/
+number.copyWithin(0, 3);
+console.log(number);
 
 
 
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+    
 
 
 
--- PrabhaK/03 - JavaScript/01 - Basics/09 - Objects.js
+++ PrabhaK/03 - JavaScript/01 - Basics/09 - Objects.js
@@ -1,12 +1,58 @@
-const person = {
-    firstname: "Prabha",
-    lastname: "kannan",
-    age: 50,
-    eyecolor: "blue"
-};
+//const person = {
+//    firstname: "Prabha",
+//    lastname: "kannan",
+//    age: 50,
+//    eyecolor: "blue"
+//};
 
-info = person.age;
-console.log(info);
-console.log("the person age is  " + info);
+//info = person.age;
+//console.log(info);
+//console.log("the person age is  " + info);
+
+//var obj = {
+//    name: "prabha" ,
+//    emailId: "kprabha0710@gmail.com",
+//phonenumber: 9790578061
+
+//}
+//console.log(obj);
+
+
+
+
+//function m()
+//{
+//    window.value = 90;
+
+//}
+//function n()
+//{
+//    console.log(window.value);
+//}
+
+//n();
+function emp(id, name, salary)
+{
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+    this.changesalary = changesalary;
+    function changesalary(othersalary)
+    {
+        this.salary = othersalary;
+    }
+
+}
+user = new emp(87, "Prabha", 300000);
+/*console.log(user.id + " " + user.name + " " + user.salary);*/
+user.changesalary(456789);
+
+console.log( user.salary);
+
+
+
+
+
+
 
 
