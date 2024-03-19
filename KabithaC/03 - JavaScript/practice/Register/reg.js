f00136c47fac218c82f035ab0470dde90f56e4b7
KabithaC <kabi.kabila.171@gmail.com>
2024-03-19 14:10:28 +0530

    Modified: Register

--- KabithaC/03 - JavaScript/practice/Register/reg.js
+++ KabithaC/03 - JavaScript/practice/Register/reg.js
@@ -1,44 +1,110 @@
-var userData = [];
+var obj = [];
 
-function registerUser()
+function submit()
 {
-    var name = document.getElementById("fname").value;
-    var gmail = document.getElementById("gmail").value;
-    var age = document.getElementById("age").value;
-    var mobileNumber = document.getElementById("phno").value;
+    var name = document.getElementById("nameInput").value;
+    var email = document.getElementById("emailInput").value;
+    var age = document.getElementById("ageInput").value;
+    var phone = document.getElementById("phoneInput").value;
 
-    var user = {
-        name: name,
-        gmail: gmail,
-        age: age,
-        mobileNumber: mobileNumber
+
+    var rowData = {
+        Name: name,
+        Email: email,
+        Age: age,
+        Phone: phone
+    };
+    obj.push(rowData);
+    updateTable();
+}
+
+function deleteRow(index)
+{
+    obj.splice(index, 1);
+    updateTable();
+}
+
+function editRow(index)
+{
+    var nameInput = document.getElementById("nameInput");
+    var emailInput = document.getElementById("emailInput");
+    var ageInput = document.getElementById("ageInput");
+    var phoneInput = document.getElementById("phoneInput");
+
+    nameInput.value = obj[index].Name;
+    emailInput.value = obj[index].Email;
+    ageInput.value = obj[index].Age;
+    phoneInput.value = obj[index].Phone;
+
+    document.getElementById("saveButton").onclick = function()
+    {
+
+        obj[index].Name = nameInput.value;
+        obj[index].Email = emailInput.value;
+        obj[index].Age = ageInput.value;
+        obj[index].Phone = phoneInput.value;
+
+
+        updateTable();
+        saveButton.parentNode.removeChild(saveButton);
     };
 
-   
-    userData.push(user);                          
-    updateUI();  
-    //deleteUI();
-
-  
+    document.body.appendChild(saveButton);
 }
 
-function updateUI()
+
+function updateTable()
 {
-    var userDataBody = document.getElementById("userDataBody");
-    userDataBody.innerHTML = " ";
-    userData.forEach(function(user,index)
+    var tableBody = document.getElementById("tableBody");
+    tableBody.innerHTML = "";
+
+
+    for(var i = 0; i < obj.length; i++)
     {
-        var row = userDataBody.insertRow(-1);
-        var nameCell = row.insertCell(0);
-        var gmailCell = row.insertCell(1);
-        var ageCell = row.insertCell(2);
-        var mobileNumberCell = row.insertCell(3);
+        var rowData = obj[i];
+        var tr = document.createElement("tr");
 
-        nameCell.textContent = user.name;
-        gmailCell.textContent = user.gmail;
-        ageCell.textContent = user.age;
-        mobileNumberCell.textContent = user.mobileNumber;
 
-    });
+        var nameCell = document.createElement("td");
+        nameCell.textContent = rowData.Name;
+        tr.appendChild(nameCell);
+
+        var emailCell = document.createElement("td");
+        emailCell.textContent = rowData.Email;
+        tr.appendChild(emailCell);
+
+        var ageCell = document.createElement("td");
+        ageCell.textContent = rowData.Age;
+        tr.appendChild(ageCell);
+
+        var phoneCell = document.createElement("td");
+        phoneCell.textContent = rowData.Phone;
+        tr.appendChild(phoneCell);
+
+        var actionsCell = document.createElement("td");
+        var editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.addEventListener("click", function(i)
+        {
+            return function()
+            {
+                editRow(i);
+            };
+        }(i));
+        actionsCell.appendChild(editButton);
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", function(i)
+        {
+            return function()
+            {
+                deleteRow(i);
+            };
+        }(i));
+        actionsCell.appendChild(deleteButton);
+
+        tr.appendChild(actionsCell);
+        tableBody.appendChild(tr);
+    }
 }
-
