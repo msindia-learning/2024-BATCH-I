49900d24d83612b8c36d28998f38cbc79fac935c
JeniferFathimaH <jeniferfathima2103@gmail.com>
2024-03-22 19:06:34 +0530

    MODIFIED: Form validation

--- JenifarFathimaH/00 - Projects/02 - JavaScript/Form validation/script.js
+++ JenifarFathimaH/00 - Projects/02 - JavaScript/Form validation/script.js
@@ -1,6 +1,4 @@
 const BASE_URL = "file:///C:/Users/ArunthandavanMullain/Desktop/jenifar-github/JenifarFathimaH/00%20-%20Projects/02%20-%20JavaScript/Form%20validation/";
-let users = [];
-
 function signupUser() {
     let username = document.getElementById("signupUsername").value;
     let email = document.getElementById("signupEmail").value;
@@ -9,26 +7,47 @@ function signupUser() {
     let password = document.getElementById("signupPassword").value;
 
     let userObj = { username: username, email: email, age: age, phone: phone, password: password };
-    users.push(userObj);
 
     document.getElementById("signupUsername").value = "";
     document.getElementById("signupEmail").value = "";
     document.getElementById("signupAge").value = "";
     document.getElementById("signupPhone").value = "";
     document.getElementById("signupPassword").value = "";
+    document.getElementById("signupMessage").innerHTML = "Sign Up Successfully!!!";
+
+    //while (users) {
+    //    currentUser = users[nIndex];
+    //    localStorage.setItem('currentUser', JSON.stringify(currentUser));   // const currentUser = JSON.stringify(currentUser));
+    //    break;
+    //}
+
+    let users = JSON.parse(localStorage.getItem("users"));
+
+    if (users == null) {
+        users = [userObj];
+    }
+    else {
+        users.push(userObj);
+    }
+
+    localStorage.setItem("users", JSON.stringify(users));
+    
     return false;
+    
 }
 
 function loginUser() {
     let username = document.getElementById("loginUsername").value;
     let password = document.getElementById("loginPassword").value;
 
-    for (let i = 0; i < users.length; i++) {
-        let currentUser = users[i];
-        if (currentUser.username === username) {
-            if (currentUser.password === password) {
-                localStorage.setItem('currentUser', JSON.stringify(currentUser));
+    let currentUser = JSON.parse(localStorage.getItem("users"));//get current user 
+    
+    for (let i = 0; i < currentUser.length; i++) {
+        let assignuser = currentUser[i];
+        if (assignuser.username === username) {
+            if (assignuser.password === password) {
                 location.href = BASE_URL + 'profile.html';
+                localStorage.setItem("users", JSON.stringify(currentUser));
                 alert("login successfully");
                 return false;
             } else {
@@ -60,4 +79,5 @@ var defaultOpen = document.getElementById("defaultOpen");
 if (defaultOpen) {
     defaultOpen.click();
 }
+
 
