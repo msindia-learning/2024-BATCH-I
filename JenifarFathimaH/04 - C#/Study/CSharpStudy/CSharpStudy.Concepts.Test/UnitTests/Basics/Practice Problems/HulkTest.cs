a2a454b5fc4875ac1c3e3df6a655d1669c5e2cdf
JeniferFathimaH <jeniferfathima2103@gmail.com>
2024-03-27 18:24:50 +0530

    ADDED: C# Practice Problems

---
+++ JenifarFathimaH/04 - C#/Study/CSharpStudy/CSharpStudy.Concepts.Test/UnitTests/Basics/Practice Problems/HulkTest.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpStudy.Concepts.Test
+{
+    [TestClass]
+    public class HulkTest
+    {
+        [TestMethod]
+        public void Test_Hulk()
+        { 
+            Hulk.Jenifar_Code(5);
+        }
+    }
+}
