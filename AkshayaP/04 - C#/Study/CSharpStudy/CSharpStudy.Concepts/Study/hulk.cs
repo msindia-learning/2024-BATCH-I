afe2ecd9b20614dc823c525bbf5b2f64ea7a555c
AkshayaP <v.bagyalakshmi1978@gmail.com>
2024-03-27 19:04:09 +0530

    ADDED:Hulk

---
+++ AkshayaP/04 - C#/Study/CSharpStudy/CSharpStudy.Concepts/Study/hulk.cs
@@ -0,0 +1,26 @@
+﻿using systems;
+
+class Hulk
+{
+    static void main(string args[])
+    {
+        Console.WriteLine("Enter the Number of Layers:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string hulkFeeling = "I hate it";
+        for(int = 0; i <= n; int++)
+        {
+            if(i % 2 == 0)
+            {
+                hulkFeeling += "that I love ";
+
+            }
+            else
+            {
+                hulkFeeling += "that I hate ";
+            }
+        }
+        hulkFeeling += "it";
+        Console.WriteLine(feeling);
+    }
+}
